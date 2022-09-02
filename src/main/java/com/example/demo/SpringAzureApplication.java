7809e542a65460685ab86ccc7dbc5154eb370161
ahmedbadry258 <ahmed.badry258@gmail.com>
2022-09-02 09:48:11 +0200

    four commit

--- src/main/java/com/example/demo/SpringAzureApplication.java
+++ src/main/java/com/example/demo/SpringAzureApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.websocket.server.PathParam;
+
 @SpringBootApplication
 @RestController
 public class SpringAzureApplication {
-	@GetMapping("/")
-	public String greeting(){
-		return "Welcome TO SpringBoot Azure Application ";
+	@GetMapping("/{name}")
+	public String greeting(@PathParam("name") String name){
+		return "Welcome " + name + " TO SpringBoot Azure Application ";
 	}
 	@GetMapping("/msg")
 	public String getMessage(){
